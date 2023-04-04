71c5b8a4a40d6bed35c47d5a9ce314065f43fd2c
Chandler Farley <chandlerfarley@live.com>
2023-04-03 19:45:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc) {
+    return Math.abs(42 - pickUpLoc);
+}
+
+function distanceFromHqInFeet(pickUpLoc) {
+    return 264 * distanceFromHqInBlocks(pickUpLoc);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocks = Math.abs(start - destination);
+    return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return ((distance - 400) * 0.02);
+    } else if (distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
